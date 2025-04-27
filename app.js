bca54976082ef05f893a5342637cfabed4757649
lieyor <yehorcurly@gmail.com>
2025-04-27 19:57:10 +0300

    add: counterDown

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const colorBg = document.getElementById("color");
 const btn = document.getElementById("btn");
+const items = document.querySelectorAll(".info_cont > h4");
+const countDownContainer = document.querySelector(".countdown")
 
 const hex = [
   "0",
@@ -37,3 +39,36 @@ function generateColor() {
 function getRandomNumber() {
     return Math.floor(Math.random() * hex.length)
 }
+
+
+let countDownDate = new Date(2026, 3, 27, 19, 56, 0).getTime()
+
+function getCountDownTime() {
+    let timeNow = new Date().getTime();
+
+
+    const distinguish = countDownDate - timeNow;
+
+    const oneDay = 24 * 60 * 60 * 1000
+    const oneHour = 60 * 60 * 1000
+    const oneMinute = 60 * 1000
+
+    let day = Math.floor(distinguish/ oneDay);
+    let hour = Math.floor((distinguish % oneDay) / oneHour)
+    let minute = Math.floor((distinguish % oneHour) / oneMinute)
+    let seconds = Math.floor((distinguish % oneMinute) / 1000)
+
+
+    const valuesOfTime = [day, hour, minute, seconds]
+
+    items.forEach(function (item, index){
+        item.textContent = valuesOfTime[index]
+    })
+
+    if (distinguish < 0){
+        clearInterval(countDownInterval)
+        countDownContainer.innerHTML = '<h4> Time was run out</h4>'
+    }
+}
+
+let countDownInterval= setInterval(getCountDownTime, 1000) 
